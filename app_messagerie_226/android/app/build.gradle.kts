63530138278bee2ead0bbb3251c9d8ca9a85713c
Nael1111 <nathanaelnael99sawadogo@gmail.com>
2026-07-23 00:15:27 +0200

    fix: set minSdk 21, disable minify, add verbose build log

--- app_messagerie_226/android/app/build.gradle.kts
+++ app_messagerie_226/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.tonorganisation.app_messagerie_226"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,9 +32,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
         }
     }
 }
